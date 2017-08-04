0618f3b9b21a3bb820bb0e0bebdb1e66d58bd55e
heng.wang <heng.wang@sao365.cn>
2017-08-04 16:59:25 +0800

    update callback

--- src/main/java/so/sao/shop/dada/response/DadaCallbackResponse.java
+++ src/main/java/so/sao/shop/dada/response/DadaCallbackResponse.java
@@ -5,32 +5,32 @@ public class DadaCallbackResponse {
     /**
      * 达达订单号
      * */
-    private String clientId;
+    private String client_id;
 
     /**
      * 商户订单号
      * */
-    private String orderId;
+    private String order_id;
 
     /**
      * 达达订单状态
      * */
-    private Integer orderStatus;
+    private Integer order_status;
 
     /**
      * 取消原因
      * */
-    private String cancelReason;
+    private String cancel_reason;
 
     /**
      * 取消原因来源
      * */
-    private Integer cancelFrom;
+    private Integer cancel_from;
 
     /**
      * 更新时间
      * */
-    private int updateTime;
+    private int update_time;
 
     /**
      * 回调签名
@@ -40,64 +40,64 @@ public class DadaCallbackResponse {
     /**
      * 达达配送员id(当配送员接单后会返回达达配送员相关信息)
      * */
-    private Integer dmId;
+    private Integer dm_id;
 
     /**
      * 达达配送员姓名(当配送员接单后会返回达达配送员相关信息)
      * */
-    private String dmName;
+    private String dm_name;
 
     /**
      * 达达配送员手机号(当配送员接单后会返回达达配送员相关信息)
      * */
-    private String dmMobile;
+    private String dm_mobile;
 
-    public String getClientId() {
-        return clientId;
+    public String getClient_id() {
+        return client_id;
     }
 
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
     }
 
-    public String getOrderId() {
-        return orderId;
+    public String getOrder_id() {
+        return order_id;
     }
 
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
+    public void setOrder_id(String order_id) {
+        this.order_id = order_id;
     }
 
-    public Integer getOrderStatus() {
-        return orderStatus;
+    public Integer getOrder_status() {
+        return order_status;
     }
 
-    public void setOrderStatus(Integer orderStatus) {
-        this.orderStatus = orderStatus;
+    public void setOrder_status(Integer order_status) {
+        this.order_status = order_status;
     }
 
-    public String getCancelReason() {
-        return cancelReason;
+    public String getCancel_reason() {
+        return cancel_reason;
     }
 
-    public void setCancelReason(String cancelReason) {
-        this.cancelReason = cancelReason;
+    public void setCancel_reason(String cancel_reason) {
+        this.cancel_reason = cancel_reason;
     }
 
-    public Integer getCancelFrom() {
-        return cancelFrom;
+    public Integer getCancel_from() {
+        return cancel_from;
     }
 
-    public void setCancelFrom(Integer cancelFrom) {
-        this.cancelFrom = cancelFrom;
+    public void setCancel_from(Integer cancel_from) {
+        this.cancel_from = cancel_from;
     }
 
-    public int getUpdateTime() {
-        return updateTime;
+    public int getUpdate_time() {
+        return update_time;
     }
 
-    public void setUpdateTime(int updateTime) {
-        this.updateTime = updateTime;
+    public void setUpdate_time(int update_time) {
+        this.update_time = update_time;
     }
 
     public String getSignature() {
@@ -108,28 +108,29 @@ public class DadaCallbackResponse {
         this.signature = signature;
     }
 
-    public Integer getDmId() {
-        return dmId;
+    public Integer getDm_id() {
+        return dm_id;
     }
 
-    public void setDmId(Integer dmId) {
-        this.dmId = dmId;
+    public void setDm_id(Integer dm_id) {
+        this.dm_id = dm_id;
     }
 
-    public String getDmName() {
-        return dmName;
+    public String getDm_name() {
+        return dm_name;
     }
 
-    public void setDmName(String dmName) {
-        this.dmName = dmName;
+    public void setDm_name(String dm_name) {
+        this.dm_name = dm_name;
     }
 
-    public String getDmMobile() {
-        return dmMobile;
+    public String getDm_mobile() {
+        return dm_mobile;
     }
 
-    public void setDmMobile(String dmMobile) {
-        this.dmMobile = dmMobile;
+    public void setDm_mobile(String dm_mobile) {
+        this.dm_mobile = dm_mobile;
     }
+
 
 }
